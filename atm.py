1d90450d5e0bcfdd9cb8f75a4bc71ad337e38153
badatga <widdefinesme@gmail.com>
2025-07-17 18:43:38 +0900

    ✨ 선택 가능한 번호 추가
    입금 (1) 출금 (2) 영수증보기 (3)ㅊ선택 가능한 조건 작성

--- atm.py
+++ atm.py
@@ -11,6 +11,12 @@ while True:
     num = input('사용하실 기능을 입력하세요 : 1.입금 2.출금 3.영수증보기 4.종료 ')
 
     if num == '4':
-        break;
+        break
+    if num == '1':
+        pass
+    if num == '2':
+        pass
+    if num == '3':
+        pass
 
 print(f'서비스를 종료합니다. 현 잔액은 {balance}')
