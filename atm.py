58f08e6d7906168362b0eaaeeefb0271ca29b39e
badatga <widdefinesme@gmail.com>
2025-07-18 13:07:54 +0900

    🎨 영수증기능

--- atm.py
+++ atm.py
@@ -5,8 +5,8 @@
 # 입금 출금 종료 입출금내역영수증 4가지 기능을 사용할 수 있는 버튼 만들기
 
 
-balance = 0;
-
+balance = 10000;
+receipts = []
 while True:
     num = input('사용하실 기능을 입력하세요 : 1.입금 2.출금 3.영수증보기 4.종료 ')
 
@@ -16,14 +16,17 @@ while True:
         deposit_amount = int(input('입금할 금액을 입력해주세요 : '))
         balance += deposit_amount
         print(f'입금하신 금액은 {deposit_amount}원이고, 현재 잔액은 {balance}원 입니다.')
+        receipts.append(('입금', deposit_amount, balance))
 
     if num == '2':
         withdraw_amount = int(input('출금할 금액을 입력해주세요 : '))
         withdraw_amount = min(balance, withdraw_amount)
         balance -= withdraw_amount
         print(f'출금금하신 금액은 {withdraw_amount}원이고, 현재 잔액은 {balance}원 입니다.')
+        receipts.append(('출금', deposit_amount, balance))
 
     if num == '3':
+        print(receipts)
         pass
 
 print(f'서비스를 종료합니다. 현재 잔액은 {balance}원 입니다')
